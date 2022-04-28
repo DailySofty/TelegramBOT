005ff40cf027f27b55381f096fd602a5847511bb
Eduardo Talarico <eduardotalarico1337@gmail.com>
2022-04-28 08:16:13 -0300

    Better Comments (2)

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #* -- Importações
 import os
 
-os.system("pip install -r requirements.txt") # Instalar requerimentos
-os.system("pause") # Envia o "pause" para o sistema (Press any key to continue . . .)
+os.system("pip install -r requirements.txt") #? Instalar requerimentos
+os.system("pause") #? Envia o "pause" para o sistema (Press any key to continue . . .)
