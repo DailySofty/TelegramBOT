6a97a5676956f77b02c136906730218b01ae5429
dudushy <eduardotalarico1337@gmail.com>
2022-10-29 19:37:28 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,40 +1,49 @@
 # pip freeze > requirements.txt
-# * -- Importações
+#* -- Importações
 import os
 import json
 import telebot
 import signal
 
-# * -- Variáveis
-path = os.path.dirname(__file__)  # ? Caminho do diretório
-token = json.load(open(f"{path}\\token.json"))["token"]  # ? Token do BOT
+#* -- Variáveis
+TITLE = "TelegramBOT"
+path = os.path.dirname(__file__) #? Caminho do diretório
+token = json.load(open(f"{path}\\token.json"))["token"] #? Token do BOT
 BOT = telebot.TeleBot(token)
 
-# * -- Funções
-def main() -> None:  # ? Função principal
+#* -- Funções
+def main() -> None: #? Função principal
     signal.signal(signal.SIGINT, confirmExit)
     
-    print("\nIniciando o BOT...")
-    BOT.polling()
+    print(f"\n[{TITLE}] Iniciando o BOT...")
+    
+    BOT.set_update_listener(detectChanges)
+    print(f"[{TITLE}] BOT iniciado.\n")
+    # BOT.polling()
+    BOT.infinity_polling()
 
 def confirmExit(signum, frame):
-    if input("\nDeseja realmente desligar o BOT? (s/n)\n>>> ") == 's':
-        print("\nBOT desligado.")
+    if input(f"\n[{TITLE}] Deseja realmente desligar o BOT? (s/n) ") == 's':
+        print(f"[{TITLE}] BOT desligado.")
         exit(1)
 
-def clearConsole() -> None:  # ? Limpa o console
+def clearConsole() -> None: #? Limpa o console
     os.system("cls" if os.name == "nt" else "clear")
 
-def returnTrue(message) -> bool:  # ? Retorna True
-    return True
+def detectChanges(messages):
+	print(f"[{TITLE}#detectChanges] detected: {messages}")
 
-@BOT.message_handler(func=returnTrue)  # ? Responde sempre que receber uma mensagem
+#? Comando /ping
+@BOT.message_handler(commands=["ping"])
+def ping(message) -> None:
+    print(f"\n[{TITLE}#ping] message: {message.text}")
+    BOT.reply_to(message, "pong")
+
+#? Responde sempre que receber uma mensagem
+@BOT.message_handler(func=lambda message: True)
 def spam(message) -> None:
+    print(f"\n[{TITLE}#spam] message: {message.text}")
     BOT.reply_to(message, "spam")
-
-@BOT.message_handler(commands=["test"])  # ? Comando /test
-def test(message) -> None:  # ? Responde a mensagem
-    BOT.reply_to(message, "Testado!")
 
 #! Main
 if __name__ == "__main__":
